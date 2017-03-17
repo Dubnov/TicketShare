9965f491e421357afd3a0d3b8b0a403ddcb0a291
chengor <cheng@Chens-Mac.local>
2017-03-17 21:03:35 +0200

    Fixed bug in the login/register button

--- TicketShare/RegisterViewController.swift
+++ TicketShare/RegisterViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class RegisterViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var lblRequiredPassword: UILabel!
+    @IBOutlet weak var lblRequiredName: UILabel!
+    @IBOutlet weak var lblRequiredEmail: UILabel!
     @IBOutlet weak var txtFullName: UITextField!
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
@@ -18,64 +21,57 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var lblBadPassword: UILabel!
     var bIsEmailValid: Bool = false
     var bIsPasswordValid: Bool = false
+    var bIsFullNameValid: Bool = false
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         txtPassword.isSecureTextEntry = true
-    }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func emailEditingDidEndValidate(_ sender: Any) {
-        if isValidEmail(testStr: self.txtEmail.text!) {
-            bIsEmailValid = true
-        }
-        else {
-            bIsEmailValid = false
-        }
-        
-        lblBadEmail.isHidden = bIsEmailValid
+        self.txtFullName.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
+        self.txtEmail.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
+        self.txtPassword.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
     }
 
-    @IBAction func passwordEditingDidEndValidate(_ sender: Any) {
-        let passLength: Int = (self.txtPassword.text?.characters.count)!
+
+    func textFieldDidChanged(_ textField: UITextField) {
         
-        if passLength >= 6 && passLength <= 12 {
-            bIsPasswordValid = true
-        } else {
-            bIsPasswordValid = false
+        switch textField {
+        case txtEmail:
+            validateEmail()
+            break
+        case txtPassword:
+            validatePassword()
+            break
+        case txtFullName:
+            validateFullName()
+            break
+        default:
+            break
         }
         
-        lblBadPassword.isHidden = bIsPasswordValid
-    }
-    
-    
-    @IBAction func registerUser(_ sender: Any) {
-        self.txtFullName.endEditing(true)
-        self.txtPassword.endEditing(true)
-        self.txtEmail.endEditing(true)
-        
-        if bIsEmailValid && bIsPasswordValid && !((txtFullName.text?.isEmpty)!) {
-            // Register - Check if email exists in db, if so - print message
-            // if not, save details and move to main page
-            let user = User(email: self.txtEmail.text!, password: self.txtPassword.text!, fullName: self.txtFullName.text!)
-            Model.instance.addUser(user: user)
-            
-            // TODO: Call this only if add user succeeded
-            self.performSegue(withIdentifier: "performSegueToMain", sender: self)
+        if (bIsEmailValid && bIsPasswordValid && bIsFullNameValid) {
+            btnRegister.isEnabled = true
+            btnRegister.alpha = 1
         } else {
-            let alertController = UIAlertController(title: "Register Error", message: "one or more of the fields are incorrect", preferredStyle: UIAlertControllerStyle.alert)
-            let alertAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default,handler: nil)
-            alertController.addAction(alertAction)
-            
-            
-            self.present(alertController, animated: true, completion: nil)
+            btnRegister.isEnabled = false
+            btnRegister.alpha = 0.5
+        }
+    }
+
+    func validateEmail() {
+        if (txtEmail.text == "") {
+            lblRequiredEmail.isHidden = false
+            lblBadEmail.isHidden = false
+            bIsEmailValid = false
+        } else if (!isValidEmail(testStr: txtEmail.text!)) {
+            lblRequiredEmail.isHidden = true
+            lblBadEmail.isHidden = false
+            bIsEmailValid = false
+        } else {
+            lblRequiredEmail.isHidden = true
+            lblBadEmail.isHidden = true
+            bIsEmailValid = true
         }
     }
     
@@ -84,6 +80,48 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: testStr)
+    }
+    
+    func validatePassword() {
+        if (txtPassword.text == "") {
+            lblRequiredPassword.isHidden = false
+            lblBadPassword.isHidden = false
+            bIsPasswordValid = false
+        } else if ((txtPassword.text?.characters.count)! < 6 ||
+                   (txtPassword.text?.characters.count)! > 12) {
+            lblRequiredPassword.isHidden = true
+            lblBadPassword.isHidden = false
+            bIsPasswordValid = false
+        } else {
+            lblRequiredPassword.isHidden = true
+            lblBadPassword.isHidden = true
+            bIsPasswordValid = true
+        }
+    }
+
+    func validateFullName() {
+        if (txtFullName.text != "") {
+            bIsFullNameValid = true
+            lblRequiredName.isHidden = true
+        } else {
+            bIsFullNameValid = false
+            lblRequiredName.isHidden = false
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func registerUser(_ sender: Any) {
+        // Register - Check if email exists in db, if so - print message
+        // if not, save details and move to main page
+        let user = User(email: self.txtEmail.text!, password: self.txtPassword.text!, fullName: self.txtFullName.text!)
+        Model.instance.addUser(user: user)
+            
+        // TODO: Call this only if add user succeeded
+        self.performSegue(withIdentifier: "performSegueToMain", sender: self)
     }
 }
 
